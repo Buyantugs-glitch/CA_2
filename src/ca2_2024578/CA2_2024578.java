1e2105e6aba6a4b8c3abb14e9a6d6de22a13be40
Buyantugs-glitch <2024578@student.cct.ie>
2025-05-09 18:55:37 +0100

    Adding import statements

--- src/ca2_2024578/CA2_2024578.java
+++ src/ca2_2024578/CA2_2024578.java
@@ -4,6 +4,13 @@
  */
 package ca2_2024578;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
 /**
  *
  * @author buyantugs
